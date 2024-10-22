c1353c163bee25f293349b324ff03812526217d4
edycyber <foodworldmeta@gmail.com>
2024-10-22 16:00:04 +0200

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,12 +1,12 @@
 function validate() {
     const code = document.getElementById('codeInput').value.trim();
     
-    const validCodes = ['AI1532', 'AI1234', 'AI9876','BP-4937','PP-59874','ASP-khgtesml45j','BPAI-65hgt33',
+    const validCodes = ['AI1532', 'AI1234', 'AI9876','BP-4937','PP-59874','ASP-khgtesml45j','BPAI-65hgt33','PPAI-27jkhg7',
                        ];
 
     if (validCodes.includes(code)) {
         window.location.href = 'app.html';
     } else {
-        window.location.href = 'https://sparrow-ai.online/premiumplan';
+        window.location.href = 'https://sparrowaionline.gumroad.com/l/premiumplanAI';
     }
 }
