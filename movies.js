7a672addb5bc6cda4c7f8b2eebd6087f1f99f801
edycyber <foodworldmeta@gmail.com>
2024-11-26 16:50:26 +0100

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,123 @@
+const aiMovieDatabase = [
+    {
+        title: "Alien",
+        description: "Comprehensive documentary about artificial intelligence revolution",
+        url: "https://www.youtube.com/embed/k5ahSzsMluI"
+    },
+    {
+        title: "Harry Potter and the Volcano Witch",
+        description: "Documentary about the current state of AI",
+        url: "https://www.youtube.com/embed/fGKAJrbiMJI"
+    },
+    {
+        title: "Harry Potter and The Founding of Hogwarts",
+        description: "Exploring the future possibilities of artificial intelligence",
+        url: "https://www.youtube.com/embed/xr1-ToY24Cg"
+    },
+    {
+        title: "Harry Potter and the Blue Ogre's Secret",
+        description: "Deep dive into machine learning technologies",
+        url: "https://www.youtube.com/embed/EneLT78nTvg"
+    },
+    {
+        title: "Star wars: The Destruction",
+        description: "Star wars: The Destruction",
+        url: "https://www.youtube.com/embed/C2pCD2FysAQ" 
+    },
+    {
+        title: "NAPOLEON",
+        description: "NAPOLEON",
+        url: "https://www.youtube.com/embed/691_gaNuThM" 
+    },
+    {
+        title: "DRAGONS",
+        description: "Dragons",
+        url: "https://www.youtube.com/embed/ZtxAD01inyo" 
+    },
+    {
+        title: "Curse of the Inca's Gold",
+        description: "Curse of the Inca's Gold",
+        url: "https://www.youtube.com/embed/E0y1M4gTej4" 
+    },
+	{
+        title: "THE TRICERATOPS",
+        description: "THE TRICERATOPS",
+        url: "https://www.youtube.com/embed/xfYLf411r6w"
+    },
+    {
+        title: "THE  BATTLE ON PARIS",
+        description: "THE  BATTLE ON PARIS",
+        url: "https://www.youtube.com/embed/YleWCSOXJJM"
+    },
+    {
+        title: "300 Spartans",
+        description: "300 Spartans",
+        url: "https://www.youtube.com/embed/BlESSYOZ5Ss"
+    },
+    {
+        title: "Cars show: The Ferrari 812 GTS ",
+        description: "Cars show: The Ferrari 812 GTS ",
+        url: "https://www.youtube.com/embed/-70gmPPeyXc"
+    },
+    {
+        title: "THE VELOCIRAPTOR",
+        description: "THE VELOCIRAPTOR
+        url: "https://www.youtube.com/embed/36FJIoYFhu0/" 
+    },
+    {
+        title: "Dracula",
+        description: "Dracula",
+        url: "https://www.youtube.com/embed/ty6uiFom7Qw" 
+    },
+    {
+        title: "Angels vs Demons. Episode 6 The Battle on Los Angeles",
+        description: "Dragons",
+        url: "https://www.youtube.com/embed/0ofv6-1X8MU" 
+    },
+    {
+        title: "Angels vs Demons. Episode 5 The Battle on New York  City",
+        description: "Curse of the Inca's Gold",
+        url: "https://www.youtube.com/embed/AI4yjQ-6mxk" 
+    },
+	{
+        title: "Angels vs Demons. Episode 4 Stanford University",
+        description: "Angels vs Demons. Episode 4 Stanford University",
+        url: "https://www.youtube.com/embed/aWII2q4Hd7M"
+    },
+    {
+        title: "Angels vs Demons. Episode  3 The Battle of Washington D.C.",
+        description: "Angels vs Demons. Episode  3 The Battle of Washington D.C.",
+        url: "https://www.youtube.com/embed/1Sl0eXOv2Vo"
+    },
+    {
+        title: "Angels vs Demons. Episode 1 The Beginning",
+        description: "Angels vs Demons. Episode 1 The Beginning",
+        url: "https://www.youtube.com/embed/yzJt60-T7Xc"
+    },
+    {
+        title: "Lionel Messi play the piano",
+        description: "Lionel Messi play the piano",
+        url: "https://www.youtube.com/embed/rOk5vbZ6Sqw" 
+    },
+    {
+        title: "Albert Einstein play the piano",
+        description: "Albert Einstein play the piano",
+        url: "https://www.youtube.com/embed/i5d9_6tzsk4" 
+    },
+    {
+        title: "Philharmonic Concert with Einstein as Principal Violinist",
+        description: "Philharmonic Concert with Einstein as Principal Violinist",
+        url: "https://www.youtube.com/embed/aUOvukJdnF8" 
+    },
+    {
+        title: "Ofeina",
+        description: "Ofeina. Ai artist",
+        url: "https://www.youtube.com/embed/62yJJbHijLE" 
+    },
+	  {
+        title: "Maddy",
+        description: "Maddy",
+        url: "https://www.youtube.com/embed/Hwh-rjeDK6A"
+    }
+    // Add more movies here...
+];
