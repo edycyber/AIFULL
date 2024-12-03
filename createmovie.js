43f318fe55724437e3e58ca14e44f1a48f793517
edycyber <foodworldmeta@gmail.com>
2024-12-03 14:39:39 +0100

    Update createmovie.js

--- createmovie.js
+++ createmovie.js
@@ -33,6 +33,46 @@ const aiMovieDatabase = [
         title: "teddy bear",
         description: "Deep dive into machine learning technologies",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/teddy%20bear.mp4?t=2024-12-03T12%3A55%3A16.383Z"
+      },
+    {
+        title: "elves",
+        description: "Comprehensive documentary about artificial intelligence revolution",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/elves.mp4?t=2024-12-03T13%3A35%3A16.158Z"
+    },
+    {
+        title: "friend",
+        description: "Documentary about the current state of AI",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/friend.mp4?t=2024-12-03T13%3A36%3A01.881Z"
+    },
+    {
+        title: "north pole",
+        description: "Exploring the future possibilities of artificial intelligence",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/North%20Pole.mp4?t=2024-12-03T13%3A36%3A53.878Z"
+    },
+    {
+        title: "game",
+        description: "Deep dive into machine learning technologies",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/game.mp4?t=2024-12-03T13%3A37%3A16.946Z"
+      },
+  {
+        title: "lego",
+        description: "Deep dive into machine learning technologies",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/lego.mp4?t=2024-12-03T13%3A37%3A36.631Z"
+      },
+  {
+        title: "cry",
+        description: "Deep dive into machine learning technologies",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/cry.mp4?t=2024-12-03T13%3A38%3A00.473Z"
+      },
+  {
+        title: "chinese new year",
+        description: "Deep dive into machine learning technologies",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/Chinese%20New%20Year.mp4?t=2024-12-03T13%3A38%3A44.539Z"
+      },
+    {
+        title: "dancing girl",
+        description: "Deep dive into machine learning technologies",
+        url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/dancing%20girl.mp4?t=2024-12-03T13%3A39%3A29.307Z"
       }
   
 ];
