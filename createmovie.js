0af96b54428137b0536a893821ddac113a10051e
edycyber <foodworldmeta@gmail.com>
2024-12-03 14:09:38 +0100

    Update createmovie.js

--- createmovie.js
+++ createmovie.js
@@ -1,36 +1,36 @@
 const aiMovieDatabase = [
     {
-        title: "create a movie about live",
+        title: "live",
         description: "Comprehensive documentary about artificial intelligence revolution",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/live%20(1).mp4?t=2024-12-03T12%3A51%3A57.392Z"
     },
     {
-        title: "create a movie about batman",
+        title: "batman",
         description: "Documentary about the current state of AI",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/batman.mp4?t=2024-12-03T12%3A52%3A50.836Z"
     },
     {
-        title: "create a movie about joker",
+        title: "joker",
         description: "Exploring the future possibilities of artificial intelligence",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/joker.mp4?t=2024-12-03T12%3A53%3A19.092Z"
     },
     {
-        title: "create a movie about manga",
+        title: "manga",
         description: "Deep dive into machine learning technologies",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/manga.mp4?t=2024-12-03T12%3A54%3A28.117Z"
       },
   {
-        title: "create a movie about model",
+        title: "model",
         description: "Deep dive into machine learning technologies",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/model.mp4?t=2024-12-03T12%3A54%3A42.466Z"
       },
   {
-        title: "create a movie about paranormal",
+        title: "paranormal",
         description: "Deep dive into machine learning technologies",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/paranormal.mp4?t=2024-12-03T12%3A54%3A58.571Z"
       },
   {
-        title: "create a movie about teddy bear",
+        title: "teddy bear",
         description: "Deep dive into machine learning technologies",
         url: "https://pwklwggrlszvorvgonca.supabase.co/storage/v1/object/public/movies/teddy%20bear.mp4?t=2024-12-03T12%3A55%3A16.383Z"
       }
